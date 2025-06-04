28b7139c652016cdac8915ec7539376a9f9cd9ee
Nhm Nazmul <125717859+nhmnazmul22@users.noreply.github.com>
2025-06-04 10:57:45 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-
-return (
+  return (
     <html lang="en">
-      <body
-        className={`${poppinsSans.className} antialiased overflow-x-hidden bg-slate-50 w-full`}
+      <body className={`${poppinsSans.className} antialiased overflow-x-hidden bg-slate-50 w-full`}>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
