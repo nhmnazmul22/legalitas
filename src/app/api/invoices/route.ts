b261c4c0e378e4d5b23ecc3783c9b3dfe1488da6
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-06-08 17:25:26 +0600

    Invoice post route done

--- src/app/api/invoices/route.ts
+++ src/app/api/invoices/route.ts
@@ -1,53 +1,71 @@
-// import { dbConnect } from "@/lib/config/db";
-// import BankModel from "@/lib/models/BankModel";
-// import { getCorsHeaders, generateInvoiceNumber } from "@/lib/utils";
-// import { NextResponse } from "next/server";
+import { dbConnect } from "@/lib/config/db";
+import BankModel from "@/lib/models/BankModel";
+import InvoiceModel from "@/lib/models/InvoiceModel";
+import { getCorsHeaders, generateInvoiceNumber } from "@/lib/utils";
+import { NextResponse } from "next/server";
 
-// // Load Database
-// const LoadDataBase = () => {
-//   dbConnect();
-// };
+// Load Database
+const LoadDataBase = () => {
+  dbConnect();
+};
 
-// LoadDataBase();
+LoadDataBase();
 
-// export const POST = async (request: Request) => {
-//   const headers = getCorsHeaders(request);
-//   try {
-//     const body = await request.json();
-//     const { service, amount, dueDate, clientId, paymentId } = body;
+export const POST = async (request: Request) => {
+  const headers = getCorsHeaders(request);
+  try {
+    const body = await request.json();
+    const { service, amount, dueDate, clientId, paymentId } = body;
 
-//     if (!service || !amount || !dueDate || !clientId || !paymentId) {
-//       return NextResponse.json(
-//         {
-//           status: "Failed",
-//           message: "Missing required fields",
-//         },
-//         {
-//           status: 400,
-//           headers: headers,
-//         }
-//       );
-//       }
-      
-//       const generateInvNo = generateInvoiceNumber();
+    if (!service || !amount || !dueDate || !clientId || !paymentId) {
+      return NextResponse.json(
+        {
+          status: "Failed",
+          message: "Missing required fields",
+        },
+        {
+          status: 400,
+          headers: headers,
+        }
+      );
+    }
 
-//     const invoiceInfo = await BankModel.create({
-//       ...body,
-//     });
-//     return NextResponse.json(
-//       { status: "Successful", data: bankInfo },
-//       {
-//         status: 201,
-//         headers: headers,
-//       }
-//     );
-//   } catch (err: any) {
-//     return NextResponse.json(
-//       { status: "Failed", message: err.toString() },
-//       {
-//         status: 500,
-//         headers: headers,
-//       }
-//     );
-//   }
-// };
+    let generateInvNo = generateInvoiceNumber();
+
+    const prevInvoice = await InvoiceModel.findOne({ invNo: generateInvNo });
+
+    if (prevInvoice) {
+      generateInvoiceNumber();
+      return NextResponse.json(
+        {
+          status: "Failed",
+          message: "A invoice here in database with this invoice no.",
+        },
+        {
+          status: 400,
+          headers: headers,
+        }
+      );
+    }
+
+    const invoiceInfo = await InvoiceModel.create({
+      ...body,
+      invNo: generateInvNo,
+    });
+    return NextResponse.json(
+      { status: "Successful", data: invoiceInfo },
+      {
+        status: 201,
+        headers: headers,
+      }
+    );
+  } catch (err: any) {
+    return NextResponse.json(
+      { status: "Failed", message: err.toString() },
+      {
+        status: 500,
+        headers: headers,
+      }
+    );
+  }
+};
